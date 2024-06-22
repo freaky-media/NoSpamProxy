e913ff68de25173580e4676131dc9e38c97ac53b
freaky-media <54108693+freaky-media@users.noreply.github.com>
2024-06-22 07:38:03 +0200

    Create NSP_CleanUpLargeFile.ps1

---
+++ NSP_CleanUpLargeFile.ps1
@@ -0,0 +1,47 @@
+﻿write-Host "---------------------------------------------" -ForegroundColor Yellow
+
+#Path to the Webportal on Server
+$filePath = "C:\Program Files\Net at Work Mail Gateway\enQsig Webportal\App_Data\Files"
+
+if (Test-Path $filePath)
+{
+#Check Folders an geht all Folder Names 
+$NSPStoreFolder = Get-ChildItem -Recurse -Force -Directory $filePath -ErrorAction SilentlyContinue
+#Count all Folders
+$NSPStoreFolderID = $NSPStoreFolder.Count
+#Output Sum
+Write-Host "Found $NSPStoreFolderID Entrys"
+
+#do for each Folder Check / Check Folder ID with NSP ID - IF Found OK - Not Found aks to delete
+foreach($NSPStoreFolderGET in $NSPStoreFolder)
+{
+$checkNSP = (Get-NspLargeFile -NameOrId $NSPStoreFolderGET).count
+
+if($checkNSP -eq $true)
+{
+#if found Output Found - can be Disabled with a # next Line
+#Write-Host "found" + $NSPStoreFolderGET -ForegroundColor Green
+}
+else
+{
+# not found in NSP
+Write-Host "NoFound" + $NSPStoreFolderGET -ForegroundColor Red
+# aks to delete from Filesystem
+$NSPConfirmDEL = Read-Host "Delte entry $NSPStoreFolderGET that not found in NSP? (type y for yes)"
+if($NSPConfirmDEL -eq 'y')
+{ 
+# remove ID from Storage 
+#Remove-NspLargeFile -Id $NSPStoreFolderGET
+Remove-Item -Path "$filePath\$NSPStoreFolderGET" -Recurse
+
+Write-Host "$filePath\$NSPStoreFolderGET is deleted" -ForegroundColor Magenta
+}
+}
+
+}
+
+
+}
+else {
+write-warning -Message "--------Pfad existiert nicht ----------------"
+}
